09f18ac2cdafd3e9561f5f8e2eb6e66762253e31
JamalMarques <yamamarques@gmail.com>
2014-08-02 23:29:19 -0300

    Revert "Revert "Subiendo el proyecto por primera vez""
    This reverts commit ccf58aefa54119d89118b246fecc62763a72beda.

---
+++ NavigationDrawerTest/src/com/example/navigationdrawertest/ItemObjetListNDraw.java
@@ -0,0 +1,19 @@
+package com.example.navigationdrawertest;
+
+public class ItemObjetListNDraw {
+	
+	private String info;
+	
+	public ItemObjetListNDraw(String inf) {
+		this.info = inf;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+}
